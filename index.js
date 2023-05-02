80ae5548b157a89a852178bc3d641381b4f137d8
tsopsericado <ricardotsopse@gmail.com>
2023-05-02 17:07:20 +0100

    some changes made in index.js

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 let slides = document.querySelectorAll('.slide-container');
 let index = 0;
+let nextbtn = document.getElementById("next")
+let prevbtn = document.getElementById("prev")
+
 
 //next function 
 function next(){
+    // console.log("1");
     slides[index].classList.remove('active');
     index = (index + 1) % slides.length;
     slides[index].classList.add('active'); 
 }
 
+nextbtn.addEventListener("click", next);
+
 //previous function 
 function prev(){
+    // console.log("-1");
     slides[index].classList.remove('active');
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active'); 
 
 }   
+prevbtn.addEventListener("click", prev);
+
 
 //autoplay
 
-setInterval(next, 7000);
+setInterval(next, 4000);
